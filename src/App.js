65fb97797998d0b4ef0a16d8104294b0221c1fcc
Krisztian Bui <buikrisz00@gmail.com>
2022-05-13 16:22:53 +0200

    code imported

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PestControl, Ozone, Ozone_temp } from './containers';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <PestControl /> }/>
+                <Route path="/ozone" element={ <Ozone_temp /> }/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
