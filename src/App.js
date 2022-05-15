2f41ff80d76e5aaa057466bfe79f4f1bcd220636
Krisztian Bui <buikrisz00@gmail.com>
2022-05-15 13:12:41 +0200

    a

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { PestControl, Ozone, Ozone_temp, Documents } from './containers';
 
 function App() {
     return (
-        <BrowserRouter basename="https://buikrisz.github.io/frugi">
+        <BrowserRouter basename="/frugi">
             <Routes>
                 <Route path="/" element={ <PestControl /> }/>
                 <Route path="/ozone" element={ <Ozone_temp /> }/>
