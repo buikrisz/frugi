6c86b83b4dafbc8d66d08bfdfe3c28e7eb030338
Krisztian Bui <buikrisz00@gmail.com>
2022-05-15 12:52:22 +0200

    basename test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { PestControl, Ozone, Ozone_temp } from './containers';
+import { PestControl, Ozone, Ozone_temp, Documents } from './containers';
 
 
 function App() {
     return (
-        <BrowserRouter basename={window.location.pathname}>
+        <BrowserRouter basename={"https://buikrisz.github.io/frugi/"}>
             <Routes>
                 <Route path="/" element={ <PestControl /> }/>
                 <Route path="/ozone" element={ <Ozone_temp /> }/>
+                <Route path="/dokumentumok" element={ <Documents /> }/>
             </Routes>
         </BrowserRouter>
     );
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as Ozone } from './Ozone';
 export { default as Ozone_temp } from './Ozone_temp';
 export { default as PestControl } from './PestControl';
+export { default as Documents } from './Documents';
