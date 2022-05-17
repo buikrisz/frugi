e0d837019bf48d812aa4767fb40b468a8233b8a4
Krisztian Bui <buikrisz00@gmail.com>
2022-05-17 13:26:39 +0200

    privacy policí new route added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { PestControl, Ozone, Ozone_temp, Documents } from './containers';
+import { PestControl, Ozone, Ozone_temp, Documents, PrivacyPolicy } from './containers';
 
 
 function App() {
     return (
-        <BrowserRouter basename="/frugi">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={ <PestControl /> }/>
                 <Route path="/ozone" element={ <Ozone /> }/>
                 <Route path="/dokumentumok" element={ <Documents /> }/>
+                <Route path="/adatvedelmi" element={ <PrivacyPolicy /> }/>
             </Routes>
         </BrowserRouter>
     );
---
+++ src/containers/PrivacyPolicy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PrivacyPolicy() {
+  return (
+    <div className='privacyPolicy'>
+        
+    </div>
+  )
+}
+
+export default PrivacyPolicy;
