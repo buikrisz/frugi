aa231e2b14c4f8b92537602287969c332880a1dc
Krisztian Bui <buikrisz00@gmail.com>
2022-05-20 17:12:25 +0200

    footer edits

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,12 +3,18 @@ import './Footer.css';
 import { BsInstagram, BsFacebook } from 'react-icons/bs';
 
 function Footer() {
-  return (
-    <footer>
-        <a href="https://www.facebook.com/FrugiOzone" target="_blank" rel="noopener noreferrer"><BsFacebook className='footerIcon' /></a>
-        <a href="https://www.instagram.com/frugiozone/" target="_blank" rel="noopener noreferrer"><BsInstagram className='footerIcon' /></a>
-    </footer>
-  )
+    return (
+        <footer>
+            <a href="https://www.facebook.com/FrugiOzone" target="_blank" rel="noopener noreferrer nofollow"><BsFacebook className='footerIcon' /></a>
+            <a href="https://www.instagram.com/frugiozone/" target="_blank" rel="noopener noreferrer nofollow"><BsInstagram className='footerIcon' /></a>
+            <h4>|</h4>
+            <div className='footerLinks'>
+                <a href='/dokumentumok#iconok'>Ikon gyűjtemény</a>
+                {/* <a href='/dokumentumok#biztonsagi'>Biztonsági adatlapok</a> */}
+                <a href='/adatvedelmi'>Adatvédelmi nyilatkozat</a>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer;
