8f155bc5d61ef854cdbf238b5105d2e5ac96b07d
Krisztian Bui <buikrisz00@gmail.com>
2023-01-13 23:01:20 +0100

    new page link added

--- src/components/PestServiceDetails/PestServiceDetails.jsx
+++ src/components/PestServiceDetails/PestServiceDetails.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import './PestServiceDetails.css';
-import { FaTimes } from 'react-icons/fa';
+import React from "react";
+import "./PestServiceDetails.css";
+import { FaTimes } from "react-icons/fa";
 
 function ServiceDetails({ img, title, text, setShowDetails }) {
-    function handleClick(e) {
-        e.target.classList.contains("serviceDetailsBg") && setShowDetails({ show: false, id: null });
-    }
-    return (
-        <div itemScope itemType="https://schema.org/ProfessionalService" className='serviceDetailsBg' onClick={handleClick}>
-            <div className='serviceDetails'>
-                <button onClick={() => setShowDetails({ show: false, id: null })}><FaTimes className='icon' /></button>
-                <img src={img} alt={img} />
-                <h4 itemProp="name">{title}</h4>
-                <h6 itemProp="description">{text}</h6>
-            </div>
-        </div>
-    )
+  function handleClick(e) {
+    e.target.classList.contains("serviceDetailsBg") &&
+      setShowDetails({ show: false, id: null });
+  }
+  return (
+    <div
+      itemScope
+      itemType="https://schema.org/ProfessionalService"
+      className="serviceDetailsBg"
+      onClick={handleClick}
+    >
+      <div className="serviceDetails">
+        <button onClick={() => setShowDetails({ show: false, id: null })}>
+          <FaTimes className="icon" />
+        </button>
+        <img src={img} alt={img} />
+        <h4 itemProp="name">{title}</h4>
+        <h6 itemProp="description">{text}</h6>
+        <a href="/szolgaltatasok.html">További információk</a>
+      </div>
+    </div>
+  );
 }
 
 export default ServiceDetails;
