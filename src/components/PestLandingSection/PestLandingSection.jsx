84c417a2576269749895cc92247713f42a7c964a
Krisztian Bui <buikrisz@gmail.com>
2023-01-25 22:56:20 +0100

    removed quotes

--- src/components/PestLandingSection/PestLandingSection.jsx
+++ src/components/PestLandingSection/PestLandingSection.jsx
@@ -1,22 +1,27 @@
-import './PestLandingSection.css';
-import { LandingForm } from '..';
-import image from '../../images/pest_control.png';
+import "./PestLandingSection.css";
+import { LandingForm } from "..";
+import image from "../../images/pest_control.png";
 
 function LandingSection() {
-    return (
-        <section id='kezdolap'>
-            <div className='landingContent'>
-                <div className='landingTextImg'>
-                    <div className='landingText'>
-                        <h2 className='landingTitle'>Lakások védelme a váratlan betolakodóktól"</h2>
-                        <h5 className='landingSubtitle'>Ágyi poloska-, csótány-, kártevőirtás magánszemélyeknek, közintézményeknek és vállalkozásoknak</h5>
-                    </div>
-                    <img src={image} alt="Pest Control" />
-                </div>
-                <LandingForm />
-            </div>
-        </section>
-    )
-}   
+  return (
+    <section id="kezdolap">
+      <div className="landingContent">
+        <div className="landingTextImg">
+          <div className="landingText">
+            <h2 className="landingTitle">
+              Lakások védelme a váratlan betolakodóktól
+            </h2>
+            <h5 className="landingSubtitle">
+              Ágyi poloska-, csótány-, kártevőirtás magánszemélyeknek,
+              közintézményeknek és vállalkozásoknak
+            </h5>
+          </div>
+          <img src={image} alt="Pest Control" />
+        </div>
+        <LandingForm />
+      </div>
+    </section>
+  );
+}
 
 export default LandingSection;
