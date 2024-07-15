a2d444380a2fc053899623149f48c4987f821379
Krisztian Bui <buikrisz@gmail.com>
2024-07-15 21:52:28 +0200

    add meta keywords

--- src/containers/PestControl.jsx
+++ src/containers/PestControl.jsx
@@ -1,41 +1,57 @@
-import React, { useState } from 'react';
-import Layout from '../layout/Layout';
-import { PestLandingSection, PestAboutSection, Pdfs, ContactSection } from '../components';
-import { CgFileDocument } from 'react-icons/cg';
-import MetaTags from 'react-meta-tags';
+import React, { useState } from "react";
+import Layout from "../layout/Layout";
+import {
+  PestLandingSection,
+  PestAboutSection,
+  Pdfs,
+  ContactSection,
+} from "../components";
+import { CgFileDocument } from "react-icons/cg";
+import MetaTags from "react-meta-tags";
 
 function PestControl() {
-    const [showPdfs, setShowPdfs] = useState(false);
+  const [showPdfs, setShowPdfs] = useState(false);
 
-    const pestServiceStructuredData = {
-        "@context": "https://schema.org",
-        "@type": "ProfessionalService",
-        headline: "Kártevőirtás",
-        description: "Minőségi kártevőirtás (ágyi poloska, csótány, patkány, darázs, hangya, moly)",
-        address: "2120, Dunakeszi Mária utca 23.",
-        email: "frugiozone@gmail.com",
-        founder: "Kovács Tibor Géza",
-        keywords: "ágyi poloska, csótány, patkány, darázs, hangya, moly, irtás, rovar, rovarirtás, kártevő, rágcsáló",
-        legalName: "Frugi-Cont Kft.",
-        telephone: "+36-70-338-4734"
-    };
+  const pestServiceStructuredData = {
+    "@context": "https://schema.org",
+    "@type": "ProfessionalService",
+    headline: "Kártevőirtás",
+    description:
+      "Minőségi kártevőirtás (ágyi poloska, csótány, patkány, darázs, hangya, moly)",
+    address: "2120, Dunakeszi Mária utca 23.",
+    email: "frugiozone@gmail.com",
+    founder: "Kovács Tibor Géza",
+    keywords:
+      "ágyi poloska, csótány, patkány, darázs, hangya, moly, irtás, rovar, rovarirtás, kártevő, rágcsáló",
+    legalName: "Frugi-Cont Kft.",
+    telephone: "+36-70-338-4734",
+  };
 
-    return (
-        <Layout>
-            <script type="application/ld+json">
-                {JSON.stringify(pestServiceStructuredData)}
-            </script>
-            <MetaTags>
-                <title>FrugiOzone Kártevőirtás</title>
-                <meta name="description" content="Hatékony és tartós kártevőirtás. Szabaduljon meg az ágyi poloska, csótány, rágcsálók, darázs, hangya és moly okozta kellemetlenségektől. Cégünk szakemberei minőségi irtószerekkel teszik kellemesebbé mindennapjaikat."/>
-            </MetaTags>
-            <PestLandingSection />
-            <PestAboutSection />
-            <button className='pdf' onClick={() => setShowPdfs(!showPdfs)}><CgFileDocument className='pdfIcon'/></button>
-            {showPdfs && <Pdfs setShowPdfs={setShowPdfs} /> }
-            <ContactSection />
-        </Layout>
-    )
+  return (
+    <Layout>
+      <script type="application/ld+json">
+        {JSON.stringify(pestServiceStructuredData)}
+      </script>
+      <MetaTags>
+        <title>FrugiOzone Kártevőirtás</title>
+        <meta
+          name="description"
+          content="Hatékony és tartós kártevőirtás. Szabaduljon meg az ágyi poloska, csótány, rágcsálók, darázs, hangya és moly okozta kellemetlenségektől. Cégünk szakemberei minőségi irtószerekkel teszik kellemesebbé mindennapjaikat."
+        />
+        <meta
+          name="keywords"
+          content="ágyi poloska, ágyi poloska csípése, ágyi poloska irtás, csotány irtás házilag, csótány irtás, rovarirtás"
+        ></meta>
+      </MetaTags>
+      <PestLandingSection />
+      <PestAboutSection />
+      <button className="pdf" onClick={() => setShowPdfs(!showPdfs)}>
+        <CgFileDocument className="pdfIcon" />
+      </button>
+      {showPdfs && <Pdfs setShowPdfs={setShowPdfs} />}
+      <ContactSection />
+    </Layout>
+  );
 }
 
 export default PestControl;
